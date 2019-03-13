03c30e0160edd62803f26696ef59a5187e9ad863
168180619 <168180619@qq.com>
2019-03-13 20:04:46 +0800

    测试一下

---
+++ caches/error_log.php
@@ -0,0 +1,19 @@
+<?php exit;?>03-11 19:15:44 | 2 | fsockopen():                   | phpcms\libs\classes\http.class.php | 84
+<?php exit;?>03-11 19:15:44 | 2 | fsockopen():                   | phpcms\libs\classes\http.class.php | 84
+<?php exit;?>03-11 19:16:07 | 2 | fsockopen():                   | phpcms\libs\classes\http.class.php | 84
+<?php exit;?>03-11 19:16:07 | 2 | fsockopen():                   | phpcms\libs\classes\http.class.php | 84
+<?php exit;?>03-11 19:17:17 | 2 | fsockopen():                   | phpcms\libs\classes\http.class.php | 84
+<?php exit;?>03-11 19:17:17 | 2 | fsockopen():                   | phpcms\libs\classes\http.class.php | 84
+<?php exit;?>03-11 22:44:40 | 2 | fsockopen():                   | phpcms\libs\classes\http.class.php | 84
+<?php exit;?>03-11 22:44:40 | 2 | fsockopen():                   | phpcms\libs\classes\http.class.php | 84
+<?php exit;?>03-11 22:50:27 | 2 | fsockopen():                   | phpcms\libs\classes\http.class.php | 84
+<?php exit;?>03-11 22:50:27 | 2 | fsockopen():                   | phpcms\libs\classes\http.class.php | 84
+<?php exit;?>03-11 22:51:42 | 2 | fsockopen():                   | phpcms\libs\classes\http.class.php | 84
+<?php exit;?>03-11 22:51:42 | 2 | fsockopen():                   | phpcms\libs\classes\http.class.php | 84
+<?php exit;?>03-11 22:54:21 | 2 | fsockopen():                   | phpcms\libs\classes\http.class.php | 84
+<?php exit;?>03-11 22:54:21 | 2 | fsockopen():                   | phpcms\libs\classes\http.class.php | 84
+<?php exit;?>03-11 23:27:50 | 2 | fsockopen():                   | phpcms\libs\classes\http.class.php | 84
+<?php exit;?>03-11 23:27:50 | 2 | fsockopen():                   | phpcms\libs\classes\http.class.php | 84
+<?php exit;?>03-11 23:32:16 | 2048 | Only variables should be passed by reference | phpcms\modules\member\index.php | 227
+<?php exit;?>03-11 23:32:26 | 2048 | Only variables should be passed by reference | phpcms\modules\member\index.php | 227
+<?php exit;?>03-12 18:43:09 | 2 | Invalid argument supplied for foreach() | phpcms\modules\wap\functions\global.func.php | 63
---
+++ phpsso_server/caches/caches_admin/caches_data/applist.cache.php
@@ -0,0 +1,16 @@
+<?php
+return array (
+  1 => 
+  array (
+    'appid' => '1',
+    'type' => 'phpcms_v9',
+    'name' => 'phpcms v9',
+    'url' => 'http://localhost/Mini_Program/',
+    'authkey' => '0aOlF789CQaZBRxuBDePHrRGgyo1gqES',
+    'ip' => '',
+    'apifilename' => 'api.php?op=phpsso',
+    'charset' => 'utf-8',
+    'synlogin' => '1',
+  ),
+);
+?>
